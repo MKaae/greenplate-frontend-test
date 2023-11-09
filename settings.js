13556399aee988a401d990e840dc6746e777f85f
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-11-09 11:19:12 +0100

    Changes url again

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "lemon-wave-0ccb79b03.4.azurestaticapps.net/api"
+export const API_URL = "https://greenplate-backend.azurewebsites.net/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"

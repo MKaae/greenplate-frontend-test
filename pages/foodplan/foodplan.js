5ad16056a045f37edd4887779a5cb61ac766e476
steffengroenandersen <steffengroenandersen@gmail.com>
2023-11-09 10:47:06 +0100

    Add foodplan

--- pages/foodplan/foodplan.js
+++ pages/foodplan/foodplan.js
@@ -1,4 +1,7 @@
+import { API_URL } from "./../../settings.js"
+import { handleHttpErrors, makeOptions, sanitizeStringWithTableRows } from "./../../utility.js"
 
+const URL = API_URL + "/recipes/limited"
 
 export async function initFoodplan(match){
     console.log("initFoodplan()")
@@ -8,6 +11,8 @@ export async function initFoodplan(match){
 }
 
 async function fetchRecipe(storeId){
-    
+        console.log(URL + "?storeId=" + storeId)
+        
+        const recipe = await fetch(URL + "?storeId=" + storeId, makeOptions("GET", null, false)).then(r =>handleHttpErrors(r))
+        console.log(recipe)
 }
-

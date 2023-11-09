9a063dd949257c58b0ecddc1b880828352c1072e
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2023-11-09 12:20:28 +0100

    temporary api text insert

--- pages/foodplan/foodplan.js
+++ pages/foodplan/foodplan.js
@@ -1,5 +1,5 @@
 import { API_URL } from "./../../settings.js"
-import { handleHttpErrors, makeOptions, sanitizeStringWithTableRows } from "./../../utility.js"
+import { handleHttpErrors, makeOptions, sanitizeStringWithTableRows, sanitizer } from "./../../utility.js"
 
 const URL = API_URL + "/recipes/limited"
 
@@ -15,4 +15,21 @@ async function fetchRecipe(storeId){
         
         const recipe = await fetch(URL + "?storeId=" + storeId, makeOptions("GET", null, false)).then(r =>handleHttpErrors(r))
         console.log(recipe)
+
+
+        // // Parse the JSON string
+        // const jsonData = JSON.parse(JSON.stringify(recipe));
+        // // Function to format JSON content into HTML
+        // console.log("recipe: ", jsonData)
+        // function formatJsonToHtml(jsonData) {
+        //     return Object.entries(jsonData)
+        //       .map(([key, value]) => `<strong>${key}:</strong><br>${value.replace(/\n/g, '<br>')}<br>`)
+        //       .join('');
+        //   }
+        //   console.log(jsonData)
+        // // Insert the formatted HTML into the card body
+        // document.getElementById('.card-text').innerHTML = formatJsonToHtml(jsonData);
+
+        document.querySelector(".card-text").innerHTML = sanitizer(JSON.stringify(recipe))
 }
+

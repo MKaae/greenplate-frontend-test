52a9ecf12e119d6b5c713b18e88556b55ae5b68d
steffengroenandersen <steffengroenandersen@gmail.com>
2023-11-13 19:09:50 +0100

    Add console.log for deployment testing

--- settings.js
+++ settings.js
@@ -1,8 +1,10 @@
 let API_URL;
 
 if (window.location.hostname === 'localhost' || window.location.hostname === "127.0.0.1"){
+    console.log("I'm using localhost!")
     API_URL = "http://localhost:8080/api"
 } else {
+    console.log("I'm using Azure")
     API_URL = "https://greenplate-backend.azurewebsites.net/api"
 }
 
